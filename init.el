988344efed5a1d2ac1a309dc1d0d0b0687749b08
Treri <treri.liu@gmail.com>
2015-11-24 10:55:05 +0800

    init commit

---
+++ init.el
@@ -0,0 +1,22 @@
+;; show line number
+(global-linum-mode)
+;; use customized linum-format: add a addition space after the line number
+(unless window-system
+  (add-hook 'linum-before-numbering-hook
+      (lambda ()
+        (setq-local linum-format-fmt
+        (let ((w (length (number-to-string
+              (count-lines (point-min) (point-max))))))
+          (concat "%" (number-to-string w) "d"))))))
+(defun linum-format-func (line)
+  (concat
+   (propertize (format linum-format-fmt line) 'face 'linum)
+   (propertize " " 'face 'mode-line)))
+(unless window-system
+  (setq linum-format 'linum-format-func))
+
+;; Use y/n instead of yes/no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Wrap long lines
+(global-visual-line-mode)
